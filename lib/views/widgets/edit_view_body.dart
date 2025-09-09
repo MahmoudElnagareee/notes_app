16ec41fb41e2110f93218e671918ec893fef726a
Mahmoud Elnagar <your-email@example.com>
2025-09-09 20:03:57 +0300

    edit notes view done

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -13,7 +13,9 @@ class EditNoteViewBody extends StatelessWidget {
         children: [
           SizedBox(height: 50),
           CustomAppBar(title: 'Edit Notes', icon: Icons.check),
+          SizedBox(height: 50),
           CustomTextField(hint: 'title'),
+          SizedBox(height: 16),
           CustomTextField(hint: 'content', maxLines: 5),
         ],
       ),
