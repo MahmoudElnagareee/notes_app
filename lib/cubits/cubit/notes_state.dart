a02cd8a7e0b870db7f28855f365acc8da1192432
Mahmoud Elnagar <your-email@example.com>
2025-09-13 17:09:55 +0300

    display notes part1

--- lib/cubits/cubit/notes_state.dart
+++ lib/cubits/cubit/notes_state.dart
@@ -5,16 +5,4 @@ sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
 
-class NotesLoading extends NotesState {}
 
-class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccess(this.notes);
-}
-
-class NotesFailure extends NotesState {
-  final String errorMessage;
-
-  NotesFailure(this.errorMessage);
-}
